952b75ffbeeb4ea148582e2f745499f2d908d44d
Mike McKay-Dirden <93532247+KastenMike@users.noreply.github.com>
2022-08-11 22:34:41 +0200

    re-add StorageClass

--- google/location.go
+++ google/location.go
@@ -30,11 +30,13 @@ func (l *Location) CreateContainer(containerName string) (stow.Container, error)
 
 	projId, _ := l.config.Config(ConfigProjectId)
 	location, _ := l.config.Config(ConfigLocation)
+	storageClass, _ := l.config.Config(ConfigStorageClass)
 
 	// Create a bucket.
 	b, err := l.client.Buckets.Insert(projId, &storage.Bucket{
-		Name:     containerName,
-		Location: location,
+		Name:         containerName,
+		Location:     location,
+		StorageClass: storageClass,
 	}).Do()
 	//res, err := l.client.Buckets.Insert(projId, &storage.Bucket{Name: containerName}).Do()
 	if err != nil {
